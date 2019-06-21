e6509cb1324f7afc1b255e769fef72ec4293ffc3
lambda x <lambda-162@users.noreply.owo.codes>
2019-06-21 04:27:20 +0000

    utils.sql.optional_connection: remove dep on async_exit_stack

--- utils/sql.py
+++ utils/sql.py
@@ -1,9 +1,9 @@
 import asyncpg
+import contextlib
 import inspect
 import re
 
 import aiocontextvars
-from async_exit_stack import AsyncExitStack
 
 from . import attrdict
 
@@ -38,26 +38,33 @@ connection.set = _connection.set
 
 def optional_connection(func):
 	"""Decorator that acquires a connection for the decorated function if the contextvar is not set."""
-	async def get_conn(self):
-		stack = AsyncExitStack()
-
-		try:
-			# allow someone to call a decorated function twice within the same Task
-			# the second time, a new connection will be acquired
-			connection().is_closed()
-		except (asyncpg.InterfaceError, LookupError):
-			connection.set(await stack.enter_async_context(self.bot.pool.acquire()))
-
-		return stack
+	class set_connection:
+		def __init__(self, pool):
+			self.pool = pool
+		async def __aenter__(self):
+			try:
+				# allow someone to call a decorated function twice within the same Task
+				# the second time, a new connection will be acquired
+				connection().is_closed()
+			except (asyncpg.InterfaceError, LookupError):
+				self.connection = conn = await self.pool.acquire()
+				connection.set(conn)
+				return conn
+			else:
+				return connection()
+		async def __aexit__(self, *excinfo):
+			with contextlib.suppress(AttributeError):
+				await self.connection.close()
 
 	if inspect.isasyncgenfunction(func):
 		async def inner(self, *args, **kwargs):
-			async with await get_conn(self):
+			async with set_connection(self.bot.pool) as conn:
+				# this does not handle two-way async gens, but i don't have any of those either
 				async for x in func(self, *args, **kwargs):
 					yield x
 	else:
 		async def inner(self, *args, **kwargs):
-			async with await get_conn(self):
+			async with set_connection(self.bot.pool) as conn:
 				return await func(self, *args, **kwargs)
 
 	return inner
