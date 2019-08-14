362f3813135baf8f244d46f42c807597b2b06a30
lambda x <lambda-162@users.noreply.owo.codes>
2019-08-14 17:09:17 +0000

    make sql/functions.coalesce_max "generic"

--- sql/functions.sql
+++ sql/functions.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION coalesce_max(x bigint, y bigint) RETURNS bigint AS $$ BEGIN
+CREATE FUNCTION coalesce_max(x anyelement, y anyelement) RETURNS anyelement AS $$ BEGIN
 	IF x IS NULL OR x < y THEN
 		RETURN y; END IF;
 	RETURN x; END $$ LANGUAGE 'plpgsql';
