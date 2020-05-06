8833f06aad8115df6740a67ba553cc3945553e4b
lambda x <lambda-162@users.noreply.owo.codes>
2020-05-06 18:07:39 +0000

    add license headers to all files

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,3 +1,18 @@
+-- © 2019 lambda#0987
+--
+-- Chrona is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.
+--
+-- Chrona is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU Affero General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with Chrona. If not, see <https://www.gnu.org/licenses/>.
+
 -- :macro get_expiry()
 -- params: channel_id
 SELECT expiry
